7f4e9d60c82325be1df7a6d53f570e072b999933
Amr <amrtsg@gmail.com>
2024-07-12 03:04:19 +0400

    added gh buttons

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ TEST_DATASET = 'bottle_basket' # Dataset to use for predictions (testing)
 # The dataset is the dataset folder name inside 'data'
 
 ## GUI ##
-USE_GRAPHS = False
+USE_GRAPHS = True
 
 ## HYPERPARAMS ##
-USE_VAL = False # Allow for validation steps during training
+USE_VAL = True # Allow for validation steps during training
 VAL_SAMPLES = 3 # Number of validation samples
 VAL_FREQUENCY = 100 # Perform validation every x step
 EPOCHS = 1000 # Number of epochs
@@ -26,4 +26,4 @@ STN_CHECK = False # Run the STN sanity check step
 MODEL_DIR = 'logs/models' # Directory to save models
 PLOTS_DIR = 'logs/plots' # Directory to save plots
 GEN_DIR = 'logs/generated' # Directory to save predictions
-TEST_EPOCH = 1 # Test saved model at X epoch
+TEST_EPOCH = 5 # Test saved model at X epoch
